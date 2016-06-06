30dc815644a9a10fe6c850a7c66fe08a10410391
Maarten De Vleeschouwer <maarten.devleeschouwer@gmail.com>
2016-06-06 19:51:31 +0200

    Start_Up

---
+++ Materiaal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Materiaal.Startup))]
+namespace Materiaal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
