0d0c9fed7e94d6640cc97cbbee462f10318b425d
Maarten De Vleeschouwer <maarten.devleeschouwer@gmail.com>
2016-06-06 20:05:50 +0200

    Artikel 06/06/2016

---
+++ Materiaal/Models/Artikel/Artikel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Materiaal.Models.Artikel
+{
+    public class Artikel
+    {
+        int art_Id { get; set; }
+        string art_Naam { get; set; }
+        int voorraad { get; set; }
+        int cat_Id { get; set; }
+
+    }
+}
---
+++ Materiaal/Models/Artikel/Categorie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Materiaal.Models.Artikel
+{
+    public class Categorie
+    {
+        int cat_Id { get; set; }
+        string cat_Naam { get; set; }
+    }
+}
